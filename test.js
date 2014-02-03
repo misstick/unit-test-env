fa4b78e1a8925dc4fa47e9bd3e12e5d71ac994a8
Noëlie Amiot <namiot@20minutes.fr>
2014-02-03 19:12:41 +0100

    Use Fake webpage.module

--- test.js
+++ test.js
@@ -1,26 +1,37 @@
 require.config({
     paths: {
-        backbone:        '/vendor/backbone-amd/backbone',
-        text:            '/vendor/requirejs-text/text'
+        jquery:          '/vendor/jquery/jquery',
+        text:            '/vendor/requirejs-text/text',
+        webpage:         '/utils/webpage'
     }
 });
 
-define(['text!/templates/article.html'], function(article) {
+define(['jquery', "webpage"], function($, webpage) {
     "use strict";
     
+    var page = webpage.create("article");
     
+    
+    // page.remove()
+    
+    // var content = _.template(page, {
+    //   title: "PLOP",
+    //   src: "/image/test.png",
+    //   cid: "c250"
+    // });
+
+
+    // plop.close();
     // including all the tests
     //  @TODO : ADD ALL PAGE HERE
-    console.log(article)
-    
-    // DEFAULT TEST
-    describe('Array', function(){
-      describe('#indexOf()', function(){
-        it('should return -1 when the value is not present', function(){
-          assert.equal(-1, [1,2,3].indexOf(5));
-          assert.equal(-1, [1,2,3].indexOf(0));
-        })
-      })
-    });
+    // // DEFAULT TEST
+    // describe('Array', function(){
+    //   describe('#indexOf()', function(){
+    //     it('should return -1 when the value is not present', function(){
+    //       assert.equal(-1, [1,2,3].indexOf(5));
+    //       assert.equal(-1, [1,2,3].indexOf(0));
+    //     })
+    //   })
+    // });
 
 });
