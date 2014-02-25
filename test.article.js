8424f639fa52fee7f76aba925ec1473e31568bc9
Noëlie Amiot <namiot@20minutes.fr>
2014-02-25 16:50:42 +0100

    Move article.test into the right file

--- test.article.js
+++ test.article.js
@@ -1,1 +1,28 @@
 // Add Her test for articles.js
+define(['jquery', "underscore", "webpage"], function($, _, webpage) {
+    "use strict";
+    
+    var page = webpage.open("article");
+
+    /*
+     - les templates utilisés (présence des attributs adéquats)
+     - unitairement les méthodes simples des vues
+     - unitairement les collection/models de données
+     - le bon envoie des événements
+    */
+
+    // page.close();
+    // including all the tests
+    //  @TODO : ADD ALL PAGE HERE
+    // // DEFAULT TEST
+    // describe('Array', function(){
+    //   describe('#indexOf()', function(){
+    //     it('should return -1 when the value is not present', function(){
+    //       assert.equal(-1, [1,2,3].indexOf(5));
+    //       assert.equal(-1, [1,2,3].indexOf(0));
+    //     })
+    //   })
+    // });
+
+    // webpage.close("article");
+});
--- test.js
+++ test.js
@@ -7,34 +7,6 @@ require.config({
     }
 });
 
-define(['jquery', "webpage"], function($, webpage) {
+define(["/test.article.js"], function() {
     "use strict";
-    
-    var page = webpage.open("article");
-    
-    // webpage.close("article");
-    
-    /*
-     - les templates utilisés (présence des attributs adéquats)
-     - unitairement les méthodes simples des vues
-     - unitairement les collection/models de données
-     - le bon envoie des événements
-    */
-
-    // page.close();
-    // including all the tests
-    //  @TODO : ADD ALL PAGE HERE
-    // // DEFAULT TEST
-    // describe('Array', function(){
-    //   describe('#indexOf()', function(){
-    //     it('should return -1 when the value is not present', function(){
-    //       assert.equal(-1, [1,2,3].indexOf(5));
-    //       assert.equal(-1, [1,2,3].indexOf(0));
-    //     })
-    //   })
-    // });
-
-    // page.remove()
-
-
 });
