69b8fd9648c9640d596b1610dd20491aebb9654b
Noëlie Amiot <namiot@20minutes.fr>
2014-02-26 14:44:02 +0100

    Make Test.article work

--- test.article.js
+++ test.article.js
@@ -1,28 +1,43 @@
 // Add Her test for articles.js
 define(['jquery', "underscore", "webpage"], function($, _, webpage) {
     "use strict";
-    
-    var page = webpage.open("article");
 
     /*
-     - les templates utilisés (présence des attributs adéquats)
+     TESTER
      - unitairement les méthodes simples des vues
      - unitairement les collection/models de données
      - le bon envoie des événements
     */
+    
+    var page, collection;
 
-    // page.close();
-    // including all the tests
-    //  @TODO : ADD ALL PAGE HERE
-    // // DEFAULT TEST
-    // describe('Array', function(){
-    //   describe('#indexOf()', function(){
-    //     it('should return -1 when the value is not present', function(){
-    //       assert.equal(-1, [1,2,3].indexOf(5));
-    //       assert.equal(-1, [1,2,3].indexOf(0));
-    //     })
-    //   })
-    // });
+    // Test des templates utilisés
+    describe('Templates Syntax', function(){
+        before(function(done){
+            page = webpage.open("article");
+            $("body").on("page:complete", function(event, data) {
+                collection = data.collection;
+                done();
+            }.bind(this))
+        });
 
-    // webpage.close("article");
+        describe('Media.Item', function(){
+
+            it('Should select Item as an illustration', function(){
+              // medias[][illustration]-> selected ?
+            });
+            
+            it('Should have Modal <markup>', function(){
+                // zoom-media-modal, delete-media-modal
+            });
+            
+            it('Picture should be draggable', function(){
+                //  data-wysiwyg-draggable="true"
+            });
+        });
+
+        after(function(){
+            webpage.close("article");
+        });
+    });
 });
